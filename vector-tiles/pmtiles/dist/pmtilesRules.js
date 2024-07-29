692911f56a2387093996ba1aa6efed1d13362448
prushfor <prushfor@L-BSC-A146390.nrn.nrcan.gc.ca>
2024-07-28 21:12:50 -0400

    Add pmtilesStyles.js usage. pmtilesStyles.js must be co-located with other modules, especially mapml.js

---
+++ vector-tiles/pmtiles/dist/pmtilesRules.js
@@ -0,0 +1,80 @@
+class SpearfishSymbolizer {
+  constructor(options) {
+    this.color = options.color;
+    this.shape = options.shape;
+  }
+  draw(context, geom, z, feature) {
+    let pt = geom[0][0];
+    context.fillStyle = this.color;
+    context.strokeStyle = this.color;
+    context.beginPath();
+    if (this.shape === 'circle') {
+      context.arc(pt.x, pt.y, 3, 0, 2 * Math.PI);
+    } else {
+      context.rect(pt.x - 2, pt.y - 2, 4, 4);
+    }
+    context.stroke();
+    context.fill();
+  }
+}
+const pmtilesRules = new Map();
+pmtilesRules.set(
+  'https://data.source.coop/protomaps/openstreetmap/tiles/v3.pmtiles',
+  { theme: { theme: 'light' } }
+);
+pmtilesRules.set(
+  'https://data.source.coop/protomaps/openstreetmap/tiles/v3.pmtiles?theme=light',
+  { theme: { theme: 'light' } }
+);
+pmtilesRules.set(
+  'https://data.source.coop/protomaps/openstreetmap/tiles/v3.pmtiles?theme=dark',
+  { theme: { theme: 'dark' } }
+);
+pmtilesRules.set(
+  'https://api.protomaps.com/tiles/v3/{z}/{x}/{y}.mvt?key=41392fb7515533a5',
+  { theme: { theme: 'light' } }
+);
+pmtilesRules.set(
+  'http://localhost:8080/geoserver/gwc/service/wmts/rest/spearfish/OSMTILE/{z}/{y}/{x}?format=application/vnd.mapbox-vector-tile',
+  {
+    rules: {
+      PAINT_RULES: [
+        {
+          dataLayer: 'streams',
+          symbolizer: new M.LineSymbolizer({ color: 'steelblue', width: 2 })
+        },
+        {
+          dataLayer: 'roads',
+          symbolizer: new M.LineSymbolizer({ color: 'maroon', width: 2 })
+        },
+        {
+          dataLayer: 'restricted',
+          symbolizer: new M.PolygonSymbolizer({
+            fill: 'red',
+            opacity: 0.5
+          })
+        },
+        {
+          dataLayer: 'restricted',
+          symbolizer: new M.LineSymbolizer({ color: 'red', width: 2 })
+        },
+        {
+          dataLayer: 'archsites',
+          symbolizer: new SpearfishSymbolizer({
+            color: 'red',
+            shape: 'square'
+          })
+        },
+        {
+          dataLayer: 'bugsites',
+          symbolizer: new SpearfishSymbolizer({
+            color: 'black',
+            shape: 'circle'
+          })
+        }
+      ],
+      LABEL_RULES: []
+    }
+  }
+);
+export { pmtilesRules };
